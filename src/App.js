f4248daf598c73494a4039467c14b520409fca8a
ami <ami.trambadia@creativecapsule.com>
2024-01-04 17:59:58 +0530

    aqi index demo

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
-// src/App.js
-import React, { useState, useEffect } from 'react';
-import L from 'leaflet';
-import 'leaflet/dist/leaflet.css';
-import Chart from 'chart.js/auto';
+import React, { useState, useEffect } from "react";
+import L from "leaflet";
+import "leaflet/dist/leaflet.css";
+import "./App.css";
 
 const App = () => {
   const [locationData, setLocationData] = useState({
@@ -10,9 +9,10 @@ const App = () => {
     longitude: null,
     city: null,
     station: null,
-    pm25: [],
+    pm25: null,
   });
   const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(true);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -24,13 +24,12 @@ const App = () => {
           longitude,
           city: data.city,
           station: data.station,
-          pm25: data.pm25
+          pm25: data.pm25,
         });
         setLoading(false);
         updateMap(latitude, longitude);
-        displayAQIChart();
       } catch (error) {
-        console.error('Error fetching data:', error);
+        console.error("Error fetching data:", error);
         setLoading(false);
       }
     };
@@ -51,89 +50,112 @@ const App = () => {
   };
 
   const fetchAQI = async (latitude, longitude) => {
-    const openAQApiKey = '6e89897acf2546e8caee2f4bc78029da75d232e7d9b79e21624065215349e13d'; // Replace with your OpenAQ API key
+    const openAQApiKey = process.env.REACT_APP_WEATHER_API_KEY;
+
     const currentDate = new Date();
-  const yesterdayDate = new Date(currentDate);
-  yesterdayDate.setDate(yesterdayDate.getDate() - 1);
-  const tomorrowDate = new Date(currentDate);
-  tomorrowDate.setDate(tomorrowDate.getDate() + 1);
+    const yesterdayDate = new Date(currentDate);
+    yesterdayDate.setDate(yesterdayDate.getDate() - 1);
+    const tomorrowDate = new Date(currentDate);
+    tomorrowDate.setDate(tomorrowDate.getDate() + 1);
 
-  const formatDate = (date) => {
-    return date.toISOString().split('.')[0] + 'Z';
-  };
+    const formatDate = (date) => {
+      return date.toISOString().split(".")[0] + "Z";
+    };
 
-   const aqiUrl = `https://api.openaq.org/v1/measurements?coordinates=${latitude},${longitude}&parameter=pm25&radius=5000&limit=1&order_by=datetime&sort=asc&api_key=${openAQApiKey}`;
+    //fetched one record from current location.
+    const aqiUrl = `https://api.openaq.org/v1/measurements?coordinates=
+    ${latitude},${longitude}&parameter=pm25&radius=5000&limit=1&order_by=datetime&sort=desc&api_key=${openAQApiKey}&format=json`;
 
-  console.log('AQI URL:', aqiUrl);
-  console.log("from",formatDate(yesterdayDate))
-  console.log("to",formatDate(tomorrowDate))
+    //I am not getting any data for current date so commenting out this
+    //     const aqiUrl = `https://api.openaq.org/v2/measurements?coordinates=
+    // ${latitude},${longitude}&date_from=${formatDate(yesterdayDate)}&date_tp=${formatDate(tomorrowDate)}parameter=pm25&radius=5000&limit=1&order_by=datetime&sort=desc&api_key=${openAQApiKey}`;
+
     const response = await fetch(aqiUrl);
     const data = await response.json();
 
-    console.log("data", data)
-
     if (data.results && data.results.length > 0) {
+      setError(false);
+
       return {
         city: data.results[0].city,
         station: data.results[0].location,
-        pm25: data.results[0].value
+        pm25: data.results[0].value,
       };
     } else {
-      throw new Error('No data available.');
+      setError(true);
+      throw new Error("No data available.");
     }
   };
 
   const updateMap = (latitude, longitude) => {
-    const map = L.map('map').setView([latitude, longitude], 13);
-    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
-    L.marker([latitude, longitude]).addTo(map).bindPopup('Your Location').openPopup();
-  };
+    const map = L.map("map").setView([latitude, longitude], 13);
+    L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png").addTo(
+      map
+    );
 
-  const displayAQIChart = () => {
-    const ctx = document.getElementById('aqi-chart').getContext('2d');
-    new Chart(ctx, {
-      type: 'line',
-      data: {
-        labels: ['Yesterday', 'Today', 'Tomorrow'],
-        datasets: [
-          {
-            label: 'PM2.5 levels',
-            data: locationData.pm25,
-            backgroundColor: 'rgba(255, 99, 132, 0.2)',
-            borderColor: 'rgba(255, 99, 132, 1)',
-            borderWidth: 1,
-          },
-        ],
-      },
-      options: {
-        scales: {
-          y: {
-            beginAtZero: true,
-          },
-        },
-      },
+    //add fetch data on click of map location
+    map.on("click", async (e) => {
+      const clickedLatitude = parseFloat(e.latlng.lat).toFixed(7);
+      const clickedLongitude = parseFloat(e.latlng.lng).toFixed(7);
+      console.log("clickedLatitude", e);
+
+      try {
+        const data = await fetchAQI(clickedLatitude, clickedLongitude);
+        setLocationData({
+          latitude: clickedLatitude,
+          longitude: clickedLongitude,
+          city: data.city,
+          station: data.station,
+          pm25: data.pm25,
+        });
+        setError(false);
+      } catch (error) {
+        setError(true);
+        console.error("Error fetching data:", error);
+      }
     });
+
+    L.marker([latitude, longitude])
+      .addTo(map)
+      .bindPopup("Your Location")
+      .openPopup();
   };
 
   return (
     <div>
       {loading ? (
-        <div>Loading...</div>
+        <div className="loader-container">
+          <div className="loader"></div>
+          <div className="loader-text">Loading...</div>
+        </div>
       ) : (
         <div>
-          <div id="map" style={{ height: '300px', marginBottom: '20px' }}></div>
-          <div>
-            <h2>Air Quality Index (AQI)</h2>
-            <p>Latitude: {locationData.latitude}</p>
-            <p>Longitude: {locationData.longitude}</p>
-            <p>City: {locationData.city}</p>
-            <p>Station: {locationData.station}</p>
-            <h3>PM2.5 levels</h3>
-            <ul>
-              {locationData.pm25}
-            </ul>
-            <canvas id="aqi-chart"></canvas>
-          </div>
+          <div id="map" className="map-container"></div>
+          {!error ? (
+            <div className="details-container">
+              <h2>Air Quality Index (AQI)</h2>
+              <p>
+                <b>Latitude:</b> {locationData.latitude}
+              </p>
+              <p>
+                <b>Longitude:</b> {locationData.longitude}
+              </p>
+              <p>
+                <b>City: </b>
+                {locationData.city}
+              </p>
+              <p>
+                <b>Station: </b>
+                {locationData.station}
+              </p>
+              <p>
+                <b>PM2.5 levels </b>
+                {locationData.pm25}
+              </p>
+            </div>
+          ) : (
+            <p class="no-data">No Data Found.</p>
+          )}
         </div>
       )}
     </div>
